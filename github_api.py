197b9df7b878f59b5f78e7faa2a0956b689250b5
Evin Bento <evin.dev009@gmail.com>
2026-06-12 15:25:44 -0400

    github api complete

--- github_api.py
+++ github_api.py
@@ -56,3 +56,83 @@ class GitHubAPI:
             
         except Exception as e:
             return {"error": str(e)}
+        
+    def get_repos(self, sort="updated", limit=10):
+        ''' get repo from your github'''
+
+        try:
+            # make GET request to /user/repos endpoint
+            url = f"{self.base_url}/user/repos"
+            
+            # paramenters for the request
+            params = {
+                "sort": sort,
+                "per_page": limit
+            }
+            
+            response = requests.get(url, headers=self.headers, params=params)
+            
+            # check if request was successful
+            if response.status_code != 200:
+                return {"error" : f"Failed to get user info: {response.status_code}"}
+        
+            # parse the data to json
+            repos = response.json()
+            
+            # returning data
+            return {
+                "repos": [ # using list comprehension [{} for repo in repos]
+                {
+                    "repo_name": repo.get("name"),
+                    "url": repo.get("html_url"),
+                    "description": repo.get("description"),
+                    "language": repo.get("language"),
+                    "stars": repo.get("stargazers_count")
+                }
+                for repo in repos
+            ]
+            }
+        
+        except Exception as e:
+            print("Error:", e)
+            
+    def get_issues(self, query, limit=10):
+        ''' search for issues across all the repos'''
+        
+        try:
+            # make GET request to /search/issues endpoint
+            url = f"{self.base_url}/search/issues"
+            
+            # github search syntax
+            search_query = f"{query} is:issue user:{self.get_user()['username']}"
+            
+            params = {
+                "q": query,
+                "per_page": limit
+            }
+
+            response = requests.get(url, headers=self.headers, params=params)
+            
+            # check if request was successful
+            if response.status_code != 200:
+                return {"error" : f"Failed to get user info: {response.status_code}"}
+            
+            data = response.json()
+            
+            return {
+                "total": data.get("total_count", 0),
+                "issues": [
+                    {
+                        "title": issue.get("title"),
+                        "repo": issue.get("repository_url").split("/")[-1],
+                        "state": issue.get("state"),
+                        "url": issue.get("html_url")
+                    }
+                    
+                    for issue in data.get("items", [])
+                ]
+            }
+        
+        except Exception as e:
+            print("Error: ", e)
+        
